73c0a22fc7c21b841b55aef6df421c8b9f4134c7
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-07-29 18:50:01 +0200

    [TASK] Update package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./gantt.scss";
 export * from "./gantt.js";
-export * from "lotivis-chart";
-export * from "lotivis-data";
-export * from "lotivis-datasets";
-export * from "lotivis-colors";
+// export * from "lotivis-chart";
+// export * from "lotivis-data";
+// export * from "lotivis-datasets";
+// export * from "lotivis-colors";
